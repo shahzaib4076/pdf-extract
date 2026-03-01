5a7045d6f365189bfe694b27cdcbb4a0871504f3
Muhammad Shahzaib Ali <shahzaibali4076@gmail.com>
2026-03-01 05:46:03 +0500

    feat: implement `ExtractService` for PDF image extraction and add `.vercel` to gitignore.

--- frontend/src/app/services/extract.service.ts
+++ frontend/src/app/services/extract.service.ts
@@ -4,8 +4,8 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ExtractService {
-    // private readonly apiUrl = 'https://pdf-extract.onrender.com/api/extract-images';
-    private readonly apiUrl = 'http://localhost:8000/api/extract-images';
+    private readonly apiUrl = 'https://pdf-extract-lrl6fm66l-shahzaibs-projects-d2576ba9.vercel.app/';
+    // private readonly apiUrl = 'http://localhost:8000/api/extract-images';
 
     constructor(private http: HttpClient) { }
 
