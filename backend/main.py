bee885796bfee52d3bf04411184406480719d773
Muhammad Shahzaib Ali <shahzaibali4076@gmail.com>
2026-03-05 17:37:12 +0500

    feat: Implement FastAPI backend to extract images from uploaded PDFs and return them as a ZIP archive.

--- backend/main.py
+++ backend/main.py
@@ -19,10 +19,11 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://pdf-img-extract.netlify.app",
-        "http://localhost:4200",
-    ],
+    # allow_origins=[
+    #     "https://pdf-img-extract.netlify.app",
+    #     "http://localhost:4200",
+    # ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
